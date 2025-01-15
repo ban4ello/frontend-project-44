7026159b9426d679d0d166007f14dce90e0103e8
ban4ello <ban4ellog@gmail.com>
2025-01-15 07:09:57 +0300

    added new game: brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { startCalculation } from '../src/cli.js';
+
+startCalculation();
--- src/cli.js
+++ src/cli.js
@@ -53,3 +53,59 @@ export const startEvenGame = () => {
 
     game();
 };
+
+export const startCalculation = () => {
+    const roundsCount = 3;
+
+    function calculate(num1, num2, operator) {
+        switch (operator) {
+            case '+':
+            return num1 + num2;
+            case '-':
+            return num1 - num2;
+            case '*':
+            return num1 * num2;
+            default:
+            return null;
+        }
+    }
+
+    function gameRound(userName) {
+        const num1 = Math.floor(Math.random() * 50);
+        const num2 = Math.floor(Math.random() * 50);
+        const operators = ['+', '-', '*'];
+        const operator = operators[Math.floor(Math.random() * operators.length)];
+
+        console.log(`Question: ${num1} ${operator} ${num2}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+        const correctAnswer = calculate(num1, num2, operator);
+        if (parseInt(userAnswer, 10) === correctAnswer) {
+            console.log('Correct!');
+            return true;
+        }
+
+
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return false;
+    }
+
+
+    function game() {
+        console.log('Welcome to the Brain Games!');
+        const userName = readlineSync.question('May I have your name? ');
+        console.log(`Hello, ${userName}!`);
+        console.log('What is the result of the expression?');
+
+        for (let i = 0; i < roundsCount; i += 1) {
+            if (!gameRound(userName)) {
+                return;
+            }
+        }
+
+        console.log(`Congratulations, ${userName}!`);
+    }
+
+
+    game();
+}
